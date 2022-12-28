ac71d61ffae25b164514ba8f9fb9dccde93bae26
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2022-12-28 12:50:30 +0530

    Functionality over Room added to sort by name, sort by date created and to hide completed tasks.

--- app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksFragment.kt
@@ -53,16 +53,16 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.action_sort_by_name -> {
-
+                viewModel.sortOrder.value = SortOrder.BY_NAME
                 return true
             }
             R.id.action_sort_by_date_created -> {
-
+                viewModel.sortOrder.value = SortOrder.BY_DATE
                 return true
             }
             R.id.action_hide_completed_tasks -> {
                 item.isChecked = !item.isChecked
-
+                viewModel.hideCompleted.value = item.isChecked
                 return true
             }
             R.id.action_delete_all_completed_tasks -> {
--- app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksViewModel.kt
@@ -5,14 +5,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.vinodpatildev.todo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 class TasksViewModel @ViewModelInject constructor(private val taskDao: TaskDao):ViewModel() {
     val searchQuery = MutableStateFlow<String>("")
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+
+    private val tasksFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query,sortOrder,hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        taskDao.getTasks(query, sortOrder, hideCompleted)
     }
     val tasks = tasksFlow.asLiveData()
 
+}
+
+enum class SortOrder{
+    BY_NAME,
+    BY_DATE
 }
