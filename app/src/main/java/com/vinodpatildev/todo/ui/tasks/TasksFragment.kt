669e12305e3a7577de1cf9d653d7675127247f2d
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2022-12-28 11:49:58 +0530

    Options menu in TasksFragment is added with with search functionality. Dao functions changed to be compatible to execute our search queries.

--- app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksFragment.kt
@@ -1,12 +1,17 @@
 package com.vinodpatildev.todo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vinodpatildev.todo.R
 import com.vinodpatildev.todo.databinding.FragmentTasksBinding
+import com.vinodpatildev.todo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,6 +34,42 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
         viewModel.tasks.observe(viewLifecycleOwner){
             tasksAdapter.submitList(it)
+        }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks,menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            // Update search query
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_sort_by_name -> {
+
+                return true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                return true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+
+                return true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksViewModel.kt
@@ -4,10 +4,15 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.vinodpatildev.todo.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 class TasksViewModel @ViewModelInject constructor(private val taskDao: TaskDao):ViewModel() {
-
-    val tasks = taskDao.getTasks().asLiveData()
+    val searchQuery = MutableStateFlow<String>("")
+    private val tasksFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+    val tasks = tasksFlow.asLiveData()
 
 }
