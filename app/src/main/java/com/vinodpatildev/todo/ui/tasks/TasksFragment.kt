784496db2f435971300739630aec79751fa83fed
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2022-12-28 09:47:07 +0530

    ListAdapter is created for recycler view and livedata observing lambda functions are written to observe Flow<Tasks> as livedata into viewmodel.

--- app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksFragment.kt
@@ -1,11 +1,34 @@
 package com.vinodpatildev.todo.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.vinodpatildev.todo.R
+import com.vinodpatildev.todo.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
     private val viewModel : TasksViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val tasksAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = tasksAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner){
+            tasksAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/vinodpatildev/todo/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package com.vinodpatildev.todo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.vinodpatildev.todo.data.TaskDao
 import javax.inject.Inject
 
 class TasksViewModel @ViewModelInject constructor(private val taskDao: TaskDao):ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 
 }
